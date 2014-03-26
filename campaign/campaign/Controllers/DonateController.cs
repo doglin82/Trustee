ff94821961b8a479bc482104ed4e4fd33b1a0fd3
karen.lin <doglin@gmail.com>
2014-03-26 14:36:57 -0400

    committing menu items
    committing menu items

---
+++ campaign/campaign/Controllers/DonateController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace campaign.Controllers
+{
+    public class DonateController : Controller
+    {
+        public ActionResult Index()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
